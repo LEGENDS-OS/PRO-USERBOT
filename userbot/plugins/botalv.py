bcf4752b60d38fefd2f15c40d6cb6c965899a187
ᴹᴿ KANNADIGA <97476963+MR-KANNADIGA@users.noreply.github.com>
2022-02-09 20:57:38 +0530

    Update botalv.py

--- userbot/plugins/botalv.py
+++ userbot/plugins/botalv.py
@@ -20,18 +20,18 @@ KANNADIGA = bot.uid
 mention = f"[{DEFAULTUSER}](tg://user?id={KANNADIGA})"
 
 
-PM_IMG = "https://telegra.ph/file/4f03f6d4e9521902eb57f.jpg"
-pm_caption = "**𝖑𝖊ɠêɳ̃dẞø✞︎ 𝙸𝚜 𝙾𝚗𝚕𝚒𝚗𝚎**\n\n"
+PM_IMG = "https://te.legra.ph/file/c085085b63638ae1ba5cf.jpg"
+pm_caption = "**𝙺𝚊𝙽𝚗𝚊𝙳𝚒𝙶𝚊 𝚋𝙾𝚝 𝙸𝚜 𝚘𝙽𝚕𝚒𝙽𝚎**\n\n"
 
-pm_caption += f"**┏🔥✞t͛ẞ̸ 𝖑𝖊ɠêɳ̃dẞø✞︎t🔥┓**\n"
-pm_caption += f"**┣🚀 𝙼𝚢 𝙼𝚊𝚜𝚝𝚎𝚛    : {mention}**\n"
-pm_caption += f"**┣🚀 𝚃𝚎𝚕𝚎𝚝𝚑𝚘𝚗 : `{version.__version__}`**\n"
-pm_caption += f"**┣🚀 𝖑𝖊ɠêɳ̃dẞø✞︎ : {KANNADIGAversion}**\n"
-pm_caption += f"**┣🚀 𝚂𝚞𝚍𝚘     : `{sudou}`**\n"
-pm_caption += f"**┣🚀 𝙾𝚠𝚗𝚎𝚛     : [𝖑𝖊ɠêɳ̃d](https://t.me/Mr_Professor_Agora)**\n"
-pm_caption += f"**┗[♦️𝙶𝚛𝚘𝚞𝚙♦️](https://t.me/NAAN_1_KANNADIGA)┛**\n"
+pm_caption += f"**┏🔥𝐊𝐀𝐍𝐍𝐀𝐃𝐈𝐆𝐀 𝐁𝐎𝐓🔥┓**\n"
+pm_caption += f"**┣🚀 𝙼𝙰𝚂𝚃𝙴𝚁  : {mention}**\n"
+pm_caption += f"**┣🚀 𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 : `{version.__version__}`**\n"
+pm_caption += f"**┣🚀 𝙺𝙰𝙽𝙽𝙰𝙳𝙸𝙶𝙰𝙱𝙾𝚃 : {KANNADIGAversion}**\n"
+pm_caption += f"**┣🚀 𝚂𝚄𝙳𝙾     : `{sudou}`**\n"
+pm_caption += f"**┣🚀 𝙾𝚆𝙽𝙴𝚁     : [𝙺𝙰𝙽𝙽𝙰𝙳𝙸𝙶𝙰](https://t.me/Mr_Professor_Agora)**\n"
+pm_caption += f"**┗[💛 𝙶𝚁𝙾𝚄𝙿 ❤️](https://t.me/NAAN_1_KANNADIGA)┛**\n"
 
-pm_caption += "    [✨яєρο✨](https://github.com/MR-KANNADIGA/KANNADIGABOT) 🔹 [📜License📜](https://github.com/MR-KANNADIGA/KANNADIGABOT/blob/master/LICENSE)"
+pm_caption += "    [✨ 𝚁𝙴𝙿𝙾 ✨](https://github.com/MR-KANNADIGA/KANNADIGABOT) 🔹 [📜License📜](https://github.com/MR-KANNADIGA/KANNADIGABOT/blob/master/LICENSE)"
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="bot$"))
