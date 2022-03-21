a7b28b64f7d40057caf904e1d3f52c7737ca8743
ᴹᴿ KANNADIGA <97476963+MR-KANNADIGA@users.noreply.github.com>
2022-03-21 13:02:29 +0530

    Update animations1.py

--- userbot/plugins/Xtra_Plugin/animations1.py
+++ userbot/plugins/Xtra_Plugin/animations1.py
@@ -141,19 +141,19 @@ async def _(event):
         event, "Calling Princess Ammu Gouda(CEO Of Telegram)......"
     )
     animation_chars = [
-        "`Connecting To AmmuGouda Palace ,India`",
+        "`Connecting To Karunada Queen,India`",
         "`Call Connected.`",
-        "`Secretary: hello This is Princess Ammu Gouda Palace. Who is this?`",
-        f"`Me: hello ma'am this is` `{DEFAULTUSER}` .Please connect me to Princess Ammu Gouda",
+        "`Secretary: hello This is Karunada Palace . Who is this?`",
+        f"`Me: hello ma'am this is` `{DEFAULTUSER}` .Please connect me to Queen SneyhShaan",
         "`User Authorised.`",
         "`Calling`  `At PRIVATE NUMBER`",
         "`Private  Call Connected...`",
         "`Me: hello ma'am, How are you ma'am!!`",
-        "`Princess AMMU GOUDA: May I Know Who Is This?`",
+        "`Queen SneyhShaan: May I Know Who Is This?`",
         f"`Me: Yes sur, itz me` `{DEFAULTUSER}`",
-        "`Princess AMMU GOUDA: OMG!!! After long time to see u, Wassup Dear...\nI'm fine btw... how are you?`",
+        "Queen SneyhShaan: OMG!!! After long time to see u, Wassup Dear...\nI'm fine btw... how are you?`",
         "`Me: I am better ma'am than good, By the way What are u Doing.`",
-        "`Princess AMMU GOUDA: Ok! I'll call you later.. I'm busy a Right Now...`",
+        "Queen SneyhShaan: Ok! I'll call you later.. I'm busy a Right Now...`",
         "`Me: Sure ma'am \nTC Bye Bye :)`",
         "`Private Call Disconnected`",
     ]
