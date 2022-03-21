7aac2cd3c1aa6bb51eddf9fd367024cf91b73d4f
ᴹᴿ KANNADIGA <97476963+MR-KANNADIGA@users.noreply.github.com>
2022-03-21 12:56:37 +0530

    Update ping.py

--- userbot/plugins/assistant/ping.py
+++ userbot/plugins/assistant/ping.py
@@ -7,17 +7,17 @@ from userbot.Config import Config
 from userbot.plugins import *
 
 KANNADIGA_IMG = os.environ.get(
-    "BOT_PING_PIC", "https://te.legra.ph/file/893de277a5c0e53cb7b44.jpg"
+    "BOT_PING_PIC", "https://te.legra.ph/file/e5b07c5154727e350b1ae.jpg"
 )
 ms = 4
 ALIVE = Config.ALIVE_NAME
 
 KannaDiga = (
-    f"**꧁•⊹٭𝙾𝚆𝙽𝙴𝚁 𝙿𝚁𝙾𝙵𝙴𝚂𝚂𝙾𝚁 𝙰𝙶𝙾𝚁𝙰٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』"
+    f"**꧁•⊹٭KARUNADA KING٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』"
 )
 
 
 @tgbot.on(events.NewMessage(pattern="^/ping"))
 async def _(event):
-    GOOD = [[Button.url("OWNER STAY HERE", "https://t.me/MASTI_IN_DOSTI")]]
+    GOOD = [[Button.url("OWNER STAY HERE", "https://t.me/Karunada_Kings_and_Queens")]]
     await tgbot.send_file(event.chat_id, KANNADIGA_IMG, caption=KannaDiga, buttons=GOOD)
